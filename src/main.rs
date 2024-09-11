6037165b774a94b9d78f868a782403dd9bd0516e
Yuta Osawa <yutaosawa81@gmail.com>
2024-09-12 06:22:42 +0900

    read source text

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
-use swc_common::comments::{CommentKind, Comments, SingleThreadedComments};
+use swc_common::comments::{Comment, CommentKind, Comments, SingleThreadedComments};
 use swc_common::sync::Lrc;
-use swc_common::Spanned;
 use swc_common::{
     errors::{ColorConfig, Handler},
     FileName, SourceMap,
 };
+use swc_common::{SourceMapper, Span};
 use swc_ecma_ast::Lit;
 use swc_ecma_parser::PResult;
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
@@ -26,15 +26,13 @@ fn parse() -> PResult<()> {
     //     .expect("failed to load test.js");
     let fm = cm.new_source_file(
         FileName::Custom("test.js".into()).into(),
-        "function foo(s = /*#123*/\"default\") {return /*#456*/'hello'}".into(),
+        "function foo(world = /*#123*/\"world\") {return /*#456*/`hello ${world}`}".into(),
     );
 
     let comments = SingleThreadedComments::default();
 
     let lexer = Lexer::new(
-        // We want to parse ecmascript
         Syntax::Es(Default::default()),
-        // EsVersion defaults to es5
         Default::default(),
         StringInput::from(&*fm),
         Some(&comments),
@@ -49,6 +47,7 @@ fn parse() -> PResult<()> {
     let strings = HashMap::new();
 
     let visitor = &mut StringBird {
+        cm: &cm,
         comments: &comments,
         strings,
     };
@@ -63,24 +62,43 @@ fn parse() -> PResult<()> {
 }
 
 struct StringBird<'a> {
+    cm: &'a SourceMap,
     comments: &'a SingleThreadedComments,
 
     strings: HashMap<String, String>,
 }
 
+impl StringBird<'_> {
+    fn get_source_text(&self, span: Span) -> String {
+        self.cm.span_to_snippet(span).unwrap()
+    }
+
+    fn get_mark_comment<'a>(
+        &self,
+        comments: &'a SingleThreadedComments,
+        span: Span,
+    ) -> Option<Comment> {
+        comments
+            .get_leading(span.lo())
+            .and_then(|c| c.last().cloned())
+            .filter(|c| c.kind == CommentKind::Block && c.text.starts_with("#"))
+    }
+}
+
 impl Visit for StringBird<'_> {
     fn visit_lit(&mut self, node: &Lit) {
         if let Lit::Str(s) = node {
-            if let Some(c) = self
-                .comments
-                .get_leading(s.span_lo())
-                .as_ref()
-                .and_then(|c| c.last())
-            {
-                if c.kind == CommentKind::Block && c.text.starts_with("#") {
-                    self.strings.insert(c.text.to_string(), s.value.to_string());
-                }
+            if let Some(c) = self.get_mark_comment(self.comments, s.span) {
+                self.strings
+                    .insert(c.text.to_string(), self.get_source_text(s.span));
             }
+        }
+    }
+
+    fn visit_tpl(&mut self, node: &swc_ecma_ast::Tpl) {
+        if let Some(c) = self.get_mark_comment(self.comments, node.span) {
+            self.strings
+                .insert(c.text.to_string(), self.get_source_text(node.span));
         }
     }
 }
