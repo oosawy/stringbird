8a6cda298b9d5f32e9c945b2366a0118480029d8
Yuta Osawa <yutaosawa81@gmail.com>
2024-09-15 01:21:49 +0900

    picking strings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
 use swc_common::comments::{Comment, CommentKind, Comments, SingleThreadedComments};
 use swc_common::sync::Lrc;
 use swc_common::{
     errors::{ColorConfig, Handler},
-    FileName, SourceMap,
+    SourceMap,
 };
 use swc_common::{SourceMapper, Span};
 use swc_ecma_ast::Lit;
@@ -11,23 +14,40 @@ use swc_ecma_parser::PResult;
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
 use swc_ecma_visit::{Visit, VisitWith};
 
-fn main() {
-    // let input = std::env::args().nth(1).expect("missing input file");
+// Represents a mapping of keys "#123" to string values "foo".
+type StringMap = HashMap<String, String>;
 
-    let _ = parse();
+fn main() {
+    let inputs = std::env::args().skip(1).collect::<Vec<_>>();
+
+    let mut dict = HashMap::new();
+
+    for input in inputs {
+        println!("Parsing {}", input);
+
+        match parse(&Path::new(&input)) {
+            Ok(map) => dict.extend(map),
+            Err(e) => eprintln!("Error: {:?}", e),
+        }
+    }
+
+    let mut output =
+        BufWriter::new(File::create("stringbird").expect("failed to create output file"));
+
+    for (k, v) in dict.iter() {
+        output.write(format!("{}={}\n", k, v).as_bytes()).unwrap();
+    }
+
+    println!("Output written to stringbird");
 }
 
-fn parse() -> PResult<()> {
+fn parse(input: &Path) -> PResult<StringMap> {
     let cm: Lrc<SourceMap> = Default::default();
     let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
 
-    // let fm = cm
-    //     .load_file(Path::new("test.js"))
-    //     .expect("failed to load test.js");
-    let fm = cm.new_source_file(
-        FileName::Custom("test.js".into()).into(),
-        "function foo(world = /*#123*/\"world\") {return /*#456*/`hello ${world}`}".into(),
-    );
+    let fm = cm
+        .load_file(input)
+        .unwrap_or_else(|e| panic!("failed to load {}: {}", input.display(), e));
 
     let comments = SingleThreadedComments::default();
 
@@ -44,28 +64,24 @@ fn parse() -> PResult<()> {
         e.into_diagnostic(&handler).emit();
     }
 
-    let strings = HashMap::new();
+    let mut strings = HashMap::new();
 
     let visitor = &mut StringBird {
         cm: &cm,
         comments: &comments,
-        strings,
+        strings: &mut strings,
     };
 
     parser.parse_program()?.visit_with(visitor);
 
-    for (k, v) in visitor.strings.iter() {
-        println!("{}: {}", k, v);
-    }
-
-    Ok(())
+    Ok(strings)
 }
 
 struct StringBird<'a> {
     cm: &'a SourceMap,
     comments: &'a SingleThreadedComments,
 
-    strings: HashMap<String, String>,
+    strings: &'a mut HashMap<String, String>,
 }
 
 impl StringBird<'_> {
@@ -90,7 +106,7 @@ impl Visit for StringBird<'_> {
         if let Lit::Str(s) = node {
             if let Some(c) = self.get_mark_comment(self.comments, s.span) {
                 self.strings
-                    .insert(c.text.to_string(), self.get_source_text(s.span));
+                    .insert(c.text[1..].to_string(), self.get_source_text(s.span));
             }
         }
     }
@@ -98,7 +114,7 @@ impl Visit for StringBird<'_> {
     fn visit_tpl(&mut self, node: &swc_ecma_ast::Tpl) {
         if let Some(c) = self.get_mark_comment(self.comments, node.span) {
             self.strings
-                .insert(c.text.to_string(), self.get_source_text(node.span));
+                .insert(c.text[1..].to_string(), self.get_source_text(node.span));
         }
     }
 }
